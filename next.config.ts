19ccb0dc5ed752136033bffdb8015f4b14bdfc35
bealecs <cliftonscottbeale@gmail.com>
2025-02-06 17:50:03 -0600

    added frontend-take-home.fetch.com to image domain for Nextjs Image element

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["frontend-take-home.fetch.com"]
+    domains: ['frontend-take-home.fetch.com']
   }
 };
 
