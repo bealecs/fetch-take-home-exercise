645b735a8e5989cff6ddf40817ffe76c8b0a6743
bealecs <cliftonscottbeale@gmail.com>
2025-02-06 21:42:57 -0600

    style tweaks to home page and added method type to fetch call

--- app/helpers/fetchDogBreeds.ts
+++ app/helpers/fetchDogBreeds.ts
@@ -1,5 +1,6 @@
 export const fetchDogBreeds = async (): Promise<string[]> => {
     const response = await fetch("https://frontend-take-home-service.fetch.com/dogs/breeds", {
+        method: "GET",
         credentials: "include",
         headers: {
             "Content-Type": "application/json"
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { AuthorizeUserForm } from "./components/AuthorizeUserForm";
 
 export default function Home() {
   return (
-    <div className="min-h-screen content-center bg-blue-800 text-blue-400">
-      <h1 className="text-6xl">Hello World</h1>
+    <div className="min-h-screen content-center items-center bg-blue-800 text-blue-400">
+      <h1 className="text-6xl text-center my-12">Fetch take home exercise</h1>
       <AuthorizeUserForm />
     </div>
   );
